69e7f81ca50ef8f849f5d90e30bae8470b21e502
David Apolinar <david.apolinar@hotmail.com>
2022-02-22 13:49:32 -0500

    git ignore files

---
+++ terraform/landing_zone/single_region/main.tf
@@ -0,0 +1,372 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.97"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+# Resource group 
+resource "azurerm_resource_group" "mon_rg" {
+  #provider = azurerm.management
+  #name                        = var.mon_resource_group_name
+  name     = "mon-core-prod-${var.location}-rg"
+  location = var.location
+  tags = {
+    "Workload"      = "Core Infra"
+    "Data Class"    = "General"
+    "Business Crit" = "Low"
+  }
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  #provider = azurerm.management
+  #name                        = var.mon_resource_group_name
+  name     = "aks-core-prod-${var.location}-rg"
+  location = var.location
+  tags = {
+    "Workload"      = "Core Infra"
+    "Data Class"    = "General"
+    "Business Crit" = "Low"
+  }
+}
+
+resource "azurerm_resource_group" "svc_rg" {
+  #provider = azurerm.management
+  name     = var.svc_resource_group_name
+  location = var.location
+  tags = {
+    "Workload"      = "Core Infra"
+    "Data Class"    = "General"
+    "Business Crit" = "High"
+  }
+}
+
+module "log_analytics" {
+  #providers = {azurerm = azurerm.management}
+  source              = "../../modules/log_analytics"
+  resource_group_name = azurerm_resource_group.mon_rg.name
+  location            = var.location
+  law_name            = "${var.law_prefix}-core-${azurerm_resource_group.mon_rg.location}-001"
+}
+
+resource "azurerm_resource_group" "hub_region1" {
+  #provider = azurerm.connectivity
+  name     = "net-core-hub-${var.region1_loc}-rg"
+  location = var.region1_loc
+  tags     = var.tags
+}
+
+module "hub_region1" {
+
+  source              = "../../modules/networking/vnet"
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  location            = azurerm_resource_group.hub_region1.location
+
+  vnet_name     = "vnet-hub-${var.region1_loc}"
+  address_space = "10.1.0.0/16"
+  # default_subnet_prefixes = ["10.1.1.0/24"]
+  dns_servers = ["168.63.129.16"]
+}
+
+module "hub_region1_default_subnet" {
+
+  source              = "../../modules/networking/subnet"
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  vnet_name           = module.hub_region1.vnet_name
+  subnet_name         = "default-subnet"
+  subnet_prefixes     = ["10.1.1.0/24"]
+  azure_fw_ip         = module.azure_firewall_region1.ip
+}
+
+resource "azurerm_ip_group" "ip_g_region1_hub" {
+  name                = "region1-hub-ipgroup"
+  location            = azurerm_resource_group.hub_region1.location
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  cidrs               = ["10.1.0.0/16"]
+
+}
+
+resource "azurerm_ip_group" "ip_g_region1_aks_spoke" {
+  name                = "region1-aks-spoke-ipgroup"
+  location            = azurerm_resource_group.hub_region1.location
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  cidrs               = ["10.3.0.0/16"]
+}
+
+resource "azurerm_ip_group" "ip_g_region1_pe_spoke" {
+  name                = "region1-pe-spoke-ipgroup"
+  location            = azurerm_resource_group.hub_region1.location
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  cidrs               = ["10.4.0.0/16"]
+}
+
+
+resource "azurerm_resource_group" "id_spk_region1" {
+  #provider = azurerm.identity
+  name     = "net-aks-spk-${var.region1_loc}-rg"
+  location = var.region1_loc
+  tags     = var.tags
+}
+
+# Create AKS spoke for region1
+module "id_spk_region1" {
+  #providers = {azurerm = azurerm.identity}
+  source              = "../../modules/networking/vnet"
+  resource_group_name = azurerm_resource_group.id_spk_region1.name
+  location            = azurerm_resource_group.id_spk_region1.location
+  vnet_name           = "vnet-aks-spk-${var.region1_loc}"
+  //address_space       = "10.3.0.0/16"
+  address_space =  "10.3.0.0/16"
+  # default_subnet_prefixes = ["10.3.1.0/24"]
+  dns_servers = [module.azure_firewall_region1.ip]
+}
+# temp spoke
+module "id_pe_region1" {
+  #providers = {azurerm = azurerm.identity}
+  source              = "../../modules/networking/vnet"
+  resource_group_name = azurerm_resource_group.id_spk_region1.name
+  location            = azurerm_resource_group.id_spk_region1.location
+  vnet_name           = "vnet-pe-spk-${var.region1_loc}"
+  address_space       = "10.4.0.0/16"
+
+  dns_servers = [module.azure_firewall_region1.ip]
+}
+
+module "id_pe_region1_default_subnet" {
+
+  source              = "../../modules/networking/subnet"
+  resource_group_name = azurerm_resource_group.id_spk_region1.name
+  vnet_name           = module.id_pe_region1.vnet_name
+  subnet_name         = "pe-subnet"
+  //subnet_prefixes = ["10.3.1.0/24"]
+  subnet_prefixes = ["10.4.1.0/24"]
+  azure_fw_ip     = module.azure_firewall_region1.ip
+}
+
+# Peering between hub1 and pe spoke
+module "peering_pe_spk_Region1_1" {
+  #providers = {azurerm = azurerm.connectivity}
+  source               = "../../modules/networking/peering_direction1"
+  resource_group_nameA = azurerm_resource_group.hub_region1.name
+  resource_group_nameB = azurerm_resource_group.id_spk_region1.name
+  netA_name            = module.hub_region1.vnet_name
+  netA_id              = module.hub_region1.vnet_id
+  netB_name            = module.id_pe_region1.vnet_name
+  netB_id              = module.id_pe_region1.vnet_id
+}
+
+# Peering between pe spoke and hub
+module "peering_spk_pe_Region1_1" {
+  #providers = {azurerm = azurerm.identity}
+  source               = "../../modules/networking/peering_direction2"
+  resource_group_nameA = azurerm_resource_group.hub_region1.name
+  resource_group_nameB = azurerm_resource_group.id_spk_region1.name
+  netA_name            = module.hub_region1.vnet_name
+  netA_id              = module.hub_region1.vnet_id
+  netB_name            = module.id_pe_region1.vnet_name
+  netB_id              = module.id_pe_region1.vnet_id
+
+  depends_on = [module.peering_pe_spk_Region1_1]
+}
+
+
+#################################
+
+module "id_spk_region1_default_subnet" {
+
+  source              = "../../modules/networking/subnet"
+  resource_group_name = azurerm_resource_group.id_spk_region1.name
+  vnet_name           = module.id_spk_region1.vnet_name
+  subnet_name         = "aks-subnet"
+  //subnet_prefixes = ["10.3.1.0/24"]
+  //subnet_prefixes = ["10.3.4.0/16"]
+  subnet_prefixes = ["10.3.0.0/16"]
+  azure_fw_ip     = module.azure_firewall_region1.ip
+}
+
+
+
+# Peering between hub1 and spk1
+module "peering_aks_spk_Region1_1" {
+  #providers = {azurerm = azurerm.connectivity}
+  source               = "../../modules/networking/peering_direction1"
+  resource_group_nameA = azurerm_resource_group.hub_region1.name
+  resource_group_nameB = azurerm_resource_group.id_spk_region1.name
+  netA_name            = module.hub_region1.vnet_name
+  netA_id              = module.hub_region1.vnet_id
+  netB_name            = module.id_spk_region1.vnet_name
+  netB_id              = module.id_spk_region1.vnet_id
+}
+
+# Peering between hub1 and spk1
+module "peering_id_spk_Region1_2" {
+  #providers = {azurerm = azurerm.identity}
+  source               = "../../modules/networking/peering_direction2"
+  resource_group_nameA = azurerm_resource_group.hub_region1.name
+  resource_group_nameB = azurerm_resource_group.id_spk_region1.name
+  netA_name            = module.hub_region1.vnet_name
+  netA_id              = module.hub_region1.vnet_id
+  netB_name            = module.id_spk_region1.vnet_name
+  netB_id              = module.id_spk_region1.vnet_id
+
+  depends_on = [module.peering_aks_spk_Region1_1]
+}
+
+module "aks" {
+  source                   = "../../modules/aks"
+  resource_group_name      = azurerm_resource_group.aks_rg.name
+  location                 = var.location
+  aks_spoke_subnet_id      = module.id_spk_region1_default_subnet.subnet_id
+  hub_virtual_network_id   = module.hub_region1.vnet_id
+  spoke_virtual_network_id = module.id_spk_region1.vnet_id
+  depends_on = [
+    module.id_spk_region1_default_subnet,
+    module.jump_host
+  ]
+  acr_id       = module.acr.acr_id
+  key_vault_id = module.hub_keyvault.kv_key_zone_id
+
+}
+
+
+
+module "acr" {
+  source              = "../../modules/acr"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = var.location
+  subnet_id           = module.id_spk_region1_default_subnet.subnet_id
+  //subnet_id                       = module.id_pe_region1_default_subnet.subnet_id
+  acr_name            = var.acr_name
+  acr_private_zone_id = module.private_dns.acr_private_zone_id
+
+}
+
+module "private_dns" {
+  source                 = "../../modules/azure_dns"
+  resource_group_name    = azurerm_resource_group.svc_rg.name
+  location               = var.location
+  hub_virtual_network_id = module.hub_region1.vnet_id
+}
+module "service_bus" {
+  source              = "../../modules/service_bus"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = var.location
+  sb-name             = var.servicebus-name
+  sb_private_zone_id  = module.private_dns.sb_private_zone_id
+  subnet_id           = module.id_spk_region1_default_subnet.subnet_id
+}
+
+module "keda_app" {
+  source              = "../../modules/keda"
+  resource_group_name = module.aks.node_resource_group
+  location            = var.location
+  sb_id               = module.service_bus.service_bus_id
+}
+
+resource "azurerm_route_table" "default_aks_route" {
+  name                = "default_aks_route"
+  resource_group_name = azurerm_resource_group.hub_region1.name
+  location            = var.location
+
+  route {
+    name                   = "default_egress"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = module.azure_firewall_region1.ip
+  }
+
+}
+
+
+# Bastion Host
+
+module "bastion_region1" {
+  #providers = {azurerm = azurerm.connectivity}
+  source                   = "../../modules/azure_bastion"
+  resource_group_name      = azurerm_resource_group.hub_region1.name
+  location                 = azurerm_resource_group.hub_region1.location
+  azurebastion_name        = var.azurebastion_name_01
+  azurebastion_vnet_name   = module.hub_region1.vnet_name
+  azurebastion_addr_prefix = "10.1.250.0/24"
+}
+
+
+
+module "azure_firewall_region1" {
+  source                  = "../../modules/azure_firewall"
+  resource_group_name     = azurerm_resource_group.hub_region1.name
+  location                = azurerm_resource_group.hub_region1.location
+  azurefw_name            = var.azurefw_name_r1
+  azurefw_vnet_name       = module.hub_region1.vnet_name
+  azurefw_addr_prefix     = var.azurefw_addr_prefix_r1
+  sc_law_id               = module.log_analytics.log_analytics_id
+  region1_aks_spk_ip_g_id = azurerm_ip_group.ip_g_region1_aks_spoke.id
+}
+
+# Jump host  Errors on creation with VMExtention is commented out
+
+module "jump_host" {
+  #providers = {azurerm = azurerm.connectivity}
+  source                    = "../../modules/jump_host"
+  resource_group_name       = azurerm_resource_group.hub_region1.name
+  location                  = azurerm_resource_group.hub_region1.location
+  jump_host_name            = var.jump_host_name
+  jump_host_vnet_name       = module.hub_region1.vnet_name
+  jump_host_addr_prefix     = var.jump_host_addr_prefix
+  jump_host_private_ip_addr = var.jump_host_private_ip_addr
+  jump_host_vm_size         = var.jump_host_vm_size
+  jump_host_admin_username  = var.jump_host_admin_username
+  jump_host_password        = var.jump_host_password
+  key_vault_id              = module.hub_keyvault.kv_key_zone_id
+  kv_rg                     = azurerm_resource_group.id_shared_region1.name
+  depends_on = [
+    azurerm_resource_group.id_shared_region1,
+    module.bastion_region1
+    
+  ]
+}
+
+
+
+resource "azurerm_resource_group" "id_shared_region1" {
+  #provider = azurerm.identity
+  name     = "shared-svc-spk-${var.region1_loc}-rg"
+  location = var.region1_loc
+  tags     = var.tags
+}
+
+/*
+#Add Additional subnets Needed
+module "id_spk_region1_shared_subnet" {
+  #providers = {azurerm = azurerm.identity}
+  source              = "../../modules/networking/subnet"
+  resource_group_name = azurerm_resource_group.id_spk_region1.name
+  vnet_name           = module.id_spk_region1.vnet_name
+  subnet_name         = "shared"
+  subnet_prefixes     = ["10.3.64.0/18"]
+  azure_fw_ip         = module.azure_firewall_region1.ip
+}
+*/
+
+#Need to fix Private Endpoint and location of DNS Zone
+
+module "hub_keyvault" {
+
+  source              = "../../modules/key_vault"
+  resource_group_name = azurerm_resource_group.id_shared_region1.name
+  location            = azurerm_resource_group.id_shared_region1.location
+  keyvault_name       = "kv-${var.corp_prefix}-${var.region1_loc}"
+  //shared_subnetid     = module.id_spk_region1_shared_subnet.subnet_id
+  shared_subnetid       = module.id_spk_region1_default_subnet.subnet_id
+  # hub_virtual_network_id = module.hub_region1.vnet_id
+  #spoke_virtual_network_id = module.id_spk_region1.vnet_id
+  kv_private_zone_id   = module.private_dns.kv_private_zone_id
+  kv_private_zone_name = module.private_dns.kv_private_zone_name
+  //depends_on           = [module.id_spk_region1_shared_subnet]
+  depends_on             = [module.id_spk_region1_default_subnet]
+}
---
+++ terraform/landing_zone/single_region/variables.tf
@@ -0,0 +1,211 @@
+variable "mon_resource_group_name" {
+  type        = string
+  description = "Azure monitoring Resource Group"
+  default     = "mon-core-prod-rg"
+}
+
+variable "svc_resource_group_name" {
+  type        = string
+  description = "Shared Services Resource Group"
+  default     = "svc-core-prod-rg"
+}
+
+variable "servicebus-name" {
+  type    = string
+  default = "dapolinasb10"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus2"
+}
+
+variable "corp_prefix" {
+  type        = string
+  description = "Corp name Prefix"
+  default     = "dapolina0"
+}
+
+# LAW module
+
+variable "law_prefix" {
+  type    = string
+  default = "law"
+}
+
+
+variable "region1_loc" {
+  default = "eastus2"
+}
+
+variable "region2_loc" {
+  default = "centralus"
+}
+
+variable "tags" {
+  description = "ARM resource tags to any resource types which accept tags"
+  type        = map(string)
+
+  default = {
+    owner = "dapolina"
+  }
+}
+
+/*
+# Windows DC Variables
+variable "compute_boot_volume_size_in_gb" {
+  description = "Boot volume size of jumpbox instance"
+  default     = 128
+}
+
+variable "enable_accelerated_networking" {
+  default = "false"
+}
+
+variable "boot_diag_SA_endpoint" {
+  default = "0"
+}
+
+variable "os_offer" {
+  default = "WindowsServer"
+}
+
+variable "os_publisher" {
+  default = "MicrosoftWindowsServer"
+}
+
+variable "os_sku" {
+  default = "2019-Datacenter"
+}
+
+variable "os_version" {
+  default = "latest"
+}
+
+variable "admin_username" {
+  default = "sysadmin"
+}
+
+#variable "admin_password" {
+#}
+
+variable "storage_account_type" {
+  default = "Standard_LRS"
+}
+*/
+
+/*
+# DSC Variables
+variable dsc_config {
+  default = "blank"
+}
+
+variable dsc_mode {
+  default = "applyAndMonitor"
+}
+*/
+
+# Azure Bastion module
+variable "azurebastion_name_01" {
+  type    = string
+  default = "corp-bastion-svc_01"
+}
+variable "azurebastion_addr_prefix" {
+  type        = string
+  description = "Azure Bastion Address Prefix"
+  default     = "10.1.250.0/24"
+}
+
+# Azure Firewall
+variable "azurefw_name_r1" {
+  type    = string
+  default = "fwhub1"
+}
+variable "azurefw_name_r2" {
+  type    = string
+  default = "fwhub2"
+}
+variable "azurefw_addr_prefix_r1" {
+  type        = string
+  description = "Azure Firewall VNET prefix"
+  default     = "10.1.254.0/24"
+}
+variable "azurefw_addr_prefix_r2" {
+  type        = string
+  description = "Azure Firewall VNET prefix"
+  default     = "10.2.254.0/24"
+}
+# ACR
+
+
+variable "acr_name" {
+  type    = string
+  default = "dapolinaacr01"
+}
+
+# Jump host1  module
+variable "jump_host_name" {
+  type    = string
+  default = "jumphostvm"
+}
+variable "jump_host_addr_prefix" {
+  type        = string
+  description = "Azure Jump Host Address Prefix"
+  default     = "10.1.251.0/24"
+}
+variable "jump_host_private_ip_addr" {
+  type        = string
+  description = "Azure Jump Host Address"
+  default     = "10.1.251.5"
+}
+variable "jump_host_vm_size" {
+  type        = string
+  description = "Azure Jump Host VM SKU"
+  default     = "Standard_DS3_v2"
+}
+variable "jump_host_admin_username" {
+  type        = string
+  description = "Azure Admin Username"
+  default     = "azureadmin"
+}
+variable "jump_host_password" {
+  sensitive = true
+  type      = string
+}
+
+
+
+
+# jumphost2
+
+
+variable "jump_host_addr_prefix2" {
+  type        = string
+  description = "Azure Jump Host Address Prefix"
+  default     = "10.2.251.0/24"
+}
+variable "jump_host_private_ip_addr2" {
+  type        = string
+  description = "Azure Jump Host Address"
+  default     = "10.2.251.5"
+}
+/*
+variable "management_subscription_id" {
+    type        = string 
+    description = "Subscription Id for Managemnet subscription"
+    default = "51444e62-8fad-49fc-affe-dade00c18dd2"
+}
+
+variable "connectivity_subscription_id" {
+    type        = string 
+    description = "Subscription Id for Connectivity subscription"
+    default = "2ea80eb6-d4a4-44f8-9bbe-4b027ad71af3"
+}
+
+variable "identity_subscription_id" {
+    type        = string 
+    description = "Subscription Id for Identity subscription"
+    default = "962d4162-7af9-411f-8b1f-3269675d8766"
+}
+
+*/
